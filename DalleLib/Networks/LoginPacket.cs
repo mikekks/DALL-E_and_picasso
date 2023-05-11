8c4c95bcf67e2c29396d893f5b135535d551b1e7
Mingyu Song <100754581+mikekks@users.noreply.github.com>
2023-05-11 18:10:27 +0900

    Dalle-E lib
    개발의 편의성을 위해 Dalle-E lib 커밋

---
+++ DalleLib/Networks/LoginPacket.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DalleLib.InGame;
+
+namespace DalleLib.Networks
+{
+    [Serializable]
+    internal class LoginPacket : Packet
+    {
+        public bool success = false;
+        User user;
+
+        public List<Room> rooms;
+        
+        public LoginPacket(int userId, string password)
+        {
+            Type = PacketType.Login;
+            user = new User(userId, password);
+        }
+
+        public LoginPacket(bool success, User user, List<Room> rooms)
+        {
+            Type = PacketType.Login;
+
+            this.success = success;
+            this.user = user;
+            this.rooms = rooms;
+        }
+    }
+}
