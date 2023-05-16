9ca96e84b7768d4a1667ee303a6d86840168596c
Mingyu Song <100754581+mikekks@users.noreply.github.com>
2023-05-16 20:46:01 +0900

    Add files via upload

---
+++ DallE_picasso/DalleLib/InGame/Room.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DalleLib.InGame
+{
+    [Serializable]
+    public class Room
+    {
+        public int roomID;
+        public int level;
+        public string roomName;
+        public int PartyNum;  // 참여인원
+        public int ReadyNum;  // Ready한 인원
+
+        // public Chat lastChat;
+
+        public Room(int roomID, int level, string roomName)
+        {
+            this.roomID = roomID;
+            this.level = level;
+            this.roomName = roomName;
+        }
+    }
+}
---
+++ DallE_picasso/DalleLib/Networks/Packet.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DalleLib.Networks
+{
+    public enum PacketType
+    {
+        Header, Close,
+        Login, DuplicateLogin, Logout, Register,
+        Answer, Chat, Room
+    }
+
+    [Serializable]
+    public class Packet
+    {
+        public const int BUFFER_SIZE = 4096;
+
+        public PacketType Type;
+
+        public Packet(PacketType Type)
+        {
+            this.Type = Type;
+        }
+
+        public Packet() { }
+
+        public static byte[] Serialize(object o)
+        {
+            MemoryStream ms = new MemoryStream();
+            new BinaryFormatter().Serialize(ms, o);
+            return ms.ToArray();
+        }
+
+        public static object Deserialize(byte[] ba)
+        {
+            MemoryStream ms = new MemoryStream(ba, 0, ba.Length);
+            return new BinaryFormatter().Deserialize(ms);
+        }
+       
+    }
+}
