9b4c14d6f997f72e4ad339a858c70a2379007d67
Mingyu Song <100754581+mikekks@users.noreply.github.com>
2023-05-16 20:46:46 +0900

    Add files via upload

---
+++ DalleLib/InGame/PlayGame.cs
@@ -0,0 +1,15 @@
+﻿using DalleLib.Networks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DalleLib.InGame
+{
+    [Serializable]
+    public class PlayGame
+    {
+
+    }
+}
---
+++ DalleLib/Networks/RoomPacket.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DalleLib.InGame;
+
+namespace DalleLib.Networks
+{
+    public enum RoomType
+    {
+        New, Enter
+    }
+
+    public class RoomPacket : Packet
+    {
+        public Room room;
+        public RoomType roomType;
+        public User user; // 방장의미
+        public Dictionary<bool, User> userList; // bool은 ready의미
+        // Quest
+
+        public RoomPacket(Room room, RoomType roomType) 
+        {
+            this.room = room;
+            this.roomType = roomType;
+            Type = PacketType.Room;
+        }
+    }
+}
