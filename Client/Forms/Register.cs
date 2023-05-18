47e1ec74b2d5e352475dcc973336c659ef829e8e
gawon1009 <barley1009@naver.com>
2023-05-19 00:53:19 +0900

    gameUI

---
+++ Client/Forms/Register.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Register_Form : Form
+    {
+        public Register_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_IDcheck_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Check_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Cancer_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_Cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+}
