4cfb441635526457ce3d334e0893887fff8acd44
jusong23 <jusong23@naver.com>
2023-05-20 01:39:12 +0900

    단일 컬럼값 리턴 테스트 중

--- Server/Classes/Database.cs
+++ Server/Classes/Database.cs
@@ -17,49 +17,65 @@ namespace Server.Classes
     static class Database
     {
 
-        public static void connectDB()
+        public static string _server = "localhost";
+        public static int _port = 3306;
+        public static string _database = "test1236";
+        public static string _id = "root";
+        public static string _pw = "00000000";
+        public static string _connectionAddress = "";
+
+        public static MySqlConnection mysql = new MySqlConnection(string.Format("Server={0};Port={1};Database={2};Uid={3};Pwd={4}", _server, _port, _database, _id, _pw));
+
+        public static Boolean connectDB()
         {
             Console.WriteLine("접근 중");
 
-            string _server = "localhost";
-            int _port = 3306;
-            string _database = "kwubuild";
-            string _id = "root";
-            string _pw = "00000000";
-            string _connectionAddress = "";
-
-            _connectionAddress = string.Format("Server={0};Port={1};Database={2};Uid={3};Pwd={4}", _server, _port, _database, _id, _pw);
 
             try
             {
-                using (MySqlConnection mysql = new MySqlConnection(_connectionAddress))
+
+                mysql.Open();
+                    // string selectQuery = string.Format("SELECT * from Users");
+                    // MySqlCommand command = new MySqlCommand(selectQuery, mysql);
+                    // MySqlDataReader table = command.ExecuteReader();
+
+
+                string query = string.Format("SELECT password FROM Users"); // userId가 test인 
+                MySqlCommand command = new MySqlCommand(query, mysql);
+                MySqlDataReader table = command.ExecuteReader();
+
+
+                while (table.Read())
                 {
-                    mysql.Open();
-                    string selectQuery = string.Format("SELECT * from Users");
-                    MySqlCommand command = new MySqlCommand(selectQuery, mysql);
-                    MySqlDataReader table = command.ExecuteReader();
+                    Console.WriteLine("1");
 
-                    while (table.Read())
-                    {
-                        Console.WriteLine("성공");
-
-                        /*
-                        ID.Text = table["userId"].ToString();
-                        TryCount.Text = table["try"].ToString();
-                        Ans.Text = table["correct"].ToString();
-                        AnsRate.Text = table["percent"].ToString();
-                        */
-                    }
-
-                    table.Close();
+                    Console.WriteLine(table["password"]);
                 }
+
+                table.Close();
+                return true;
             }
             catch (Exception exc)
             {
                 Console.WriteLine("실패");
+                return false;
             }
 
         }
+
+        public static Boolean loginDB(string userId, string password)
+        { // 로그인 유저 있으면 true 없으면 false
+
+            string query = string.Format("SELECT password FROM Users WHRER Users.userId = {0}",userId); // userId가 test인 
+//            string query = string.Format("SELECT  FROM {0}", tableName);
+
+            MySqlCommand command = new MySqlCommand(query, mysql);
+
+
+            return false;
+
+        }
+
     }
 }
 
